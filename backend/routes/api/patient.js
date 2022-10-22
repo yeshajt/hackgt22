9b178c8e55ec283cadacc26092a9e96159ccf280
rreimoo3 <reza.reimoo@gatech.edu>
2022-10-22 14:52:03 -0400

    halp with mongo issue

--- backend/routes/api/patient.js
+++ backend/routes/api/patient.js
@@ -1,6 +1,19 @@
 const express = require("express");
 const router = express.Router();
 
+const Room = require("../../models/Room");
+
 router.get("/", (req, res) => res.send("Patient Route"));
+
+router.get("/find", async function (req, res) {
+  let num = req.query.roomNum;
+
+  const room = await Room.find();
+
+  if (!room) {
+    return res.status(400).json({ msg: "Nope" });
+  }
+  res.json(room);
+});
 
 module.exports = router;
