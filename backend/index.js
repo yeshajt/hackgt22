df91ae687d0205101ed429920a511e5cfb4c6fe6
Yesha Thakkar <yeshat@lawn-128-61-15-17.lawn.gatech.edu>
2022-10-22 14:55:52 -0400

    api endpoint backend changes

--- backend/index.js
+++ backend/index.js
@@ -4,6 +4,10 @@ app = express();
 
 app.get("/", (req, res) => res.send("API Sending"));
 
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 3001;
+
+app.get("/api", (req, res) => {
+    res.json({ message: "Hello from server!" });
+  });
 
 app.listen(PORT, () => console.log(`Server is running on ${PORT}`));
