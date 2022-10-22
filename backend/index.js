063cd012b5b476b69124b6edc1cd0a157a690774
rreimoo3 <reza.reimoo@gatech.edu>
2022-10-22 12:09:19 -0400

    fix port error

--- backend/index.js
+++ backend/index.js
@@ -4,6 +4,6 @@ app = express();
 
 app.get("/", (req, res) => res.send("API Sending"));
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
 
 app.listen(PORT, () => console.log(`Server is running on ${PORT}`));
